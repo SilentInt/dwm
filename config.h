1c55fb5a4937d42a5f02e64b3c5db83238e6be43
SilentInt <silentint@foxmail.com>
2022-08-21 19:19:38 +0800

    Modifyed in arch

--- config.h
+++ config.h
@@ -17,7 +17,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
+static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=14" ,
+					"WenQuanYi Micro Hei:size=14:type=Regular:antialias=true:autohint=true"};
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
